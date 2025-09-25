b7aece8bf8a7154ce4e7d55ac39de9167b38a613
hsugvusrihgvir <dariasugarrrrr@gmail.com>
2025-09-25 11:25:43 +0300

    readme

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from app.ui.main_window import MainWindow
+from app.main_window import MainWindow
 
 def main():
     app = QApplication(sys.argv)
